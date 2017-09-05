305ada4d84c683178dca5d3697d21877d86e613c
Vixentael <vixentael@gmail.com>
2017-09-05 18:47:05 +0300

    add xcode8, xcode9 projects

---
+++ xcode8/TestFrameworkXcode8/TestFrameworkXcode8/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestFrameworkXcode8
+//
+//  Created by Anastasiia on 9/5/17.
+//
+//
+
+import UIKit
+import openssl
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
